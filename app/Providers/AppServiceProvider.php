15e352945ee0617a494815e3007aa623aff6c66f
AmmaarSiraj <ammarsirajnomor1pl@gmail.com>
2026-07-04 14:42:39 +0700

    tes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Route::middleware('auth')->get('/storage/{path}', function (string $path) {
             $disk = Storage::disk('public');
 
