bf66681214f2909982a589f4082127dd7241f8b8
AmmaarSiraj <ammarsirajnomor1pl@gmail.com>
2026-07-04 14:27:12 +0700

    revisi

--- api/index.php
+++ api/index.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Filesystem\FilesystemServiceProvider;
+use Illuminate\Translation\TranslationServiceProvider;
+use Illuminate\View\ViewServiceProvider;
 
 define('LARAVEL_START', microtime(true));
 
@@ -36,5 +39,19 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $app->useStoragePath($storagePath);
+
+/*
+|--------------------------------------------------------------------------
+| Vercel Runtime Provider Fallback
+|--------------------------------------------------------------------------
+|
+| Vercel serverless runtime kadang belum memuat binding view sebelum error
+| handler mencoba render halaman error. Ini memastikan service view tersedia.
+|
+*/
+
+$app->register(FilesystemServiceProvider::class);
+$app->register(TranslationServiceProvider::class);
+$app->register(ViewServiceProvider::class);
 
 $app->handleRequest(Request::capture());
