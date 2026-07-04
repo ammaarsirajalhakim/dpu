3926e249f0100edc645863664bed2f1dd327bf63
AmmaarSiraj <ammarsirajnomor1pl@gmail.com>
2026-07-04 12:33:09 +0700

    set

--- api/index.php
+++ api/index.php
@@ -5,6 +5,27 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+$storagePath = $_ENV['APP_STORAGE_PATH'] ?? $_SERVER['APP_STORAGE_PATH'] ?? '/tmp/storage';
+$viewCompiledPath = $_ENV['VIEW_COMPILED_PATH'] ?? $_SERVER['VIEW_COMPILED_PATH'] ?? $storagePath . '/framework/views';
+
+foreach ([
+    $storagePath,
+    $storagePath . '/app',
+    $storagePath . '/app/public',
+    $storagePath . '/framework',
+    $storagePath . '/framework/cache',
+    $storagePath . '/framework/cache/data',
+    $storagePath . '/framework/sessions',
+    $viewCompiledPath,
+    $storagePath . '/logs',
+] as $path) {
+    if (! is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+}
+
+$_ENV['VIEW_COMPILED_PATH'] = $_SERVER['VIEW_COMPILED_PATH'] = $viewCompiledPath;
+
 if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
@@ -13,5 +34,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 /** @var Application $app */
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath($storagePath);
 
 $app->handleRequest(Request::capture());
